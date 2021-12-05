b053f601ef3470c98794ec2029fef275240021a8
Frorio <frorio@protonmail.ch>
2021-12-06 00:39:51 +0300

    Update 09_Tip_Calc.py

--- 2_Day/09_Tip_Calc.py
+++ 2_Day/09_Tip_Calc.py
@@ -2,11 +2,16 @@
 #Each person should pay (150.00 / 5) * 1.12 = 33.6
 #Round the result to 2 decimal places
 
+from typing import final
+
+
 bill = float(input("Enter your bill: "))
 tip = bill * float(input("Enter tip you want to pay in %: "))
 
 final_bill = bill + tip / 100
+final_bill = round(final_bill, 2)
 print(f"Your bill with tip will be: {final_bill}$")
 
-splited_bill = round(final_bill / int(input("How many people will pay?: ")), 2)
+splited_bill = final_bill / int(input("How many people will pay?: "))
+splited_bill = round(splited_bill, 2)
 print(f"Each of you will pay: {splited_bill}")
